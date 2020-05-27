dfbf4666f5f3c15323852e97638e1ae70f03ed35
LucasVillarim <villarim94@gmail.com>
2020-05-27 20:04:02 -0300

    Some import fixes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import scrollToTop from './scrollToTop';
 
 import Home from './pages/Home/index';
 import Header from './header/index';
